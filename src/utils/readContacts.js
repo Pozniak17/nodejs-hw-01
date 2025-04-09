2949f228c5874f86d138d3b68b8db24acf87f670
Yevhenii Pozniak <jekamanu@gmail.com>
2025-04-09 17:08:57 +0300

    update

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const contacts = await fs.readFile(PATH_DB, { encoding: 'utf-8' });
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.error('Помилка читання файлу:', error);
+  }
+};
+
+readContacts().then((data) => console.log(data));
